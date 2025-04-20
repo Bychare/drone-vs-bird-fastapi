00cb74e0a48e1d0791486815f67da2f025f0ff7e
Bychare <117040086+Bychare@users.noreply.github.com>
2025-04-20 17:40:40 +0300

    Update image_utils.py

--- utils/image_utils.py
+++ utils/image_utils.py
@@ -1,1 +1,9 @@
+import os
+import shutil
 
+def save_uploaded_file(file, file_id):
+    os.makedirs("static", exist_ok=True)
+    input_path = f"static/{file_id}.jpg"
+    with open(input_path, "wb") as buffer:
+        shutil.copyfileobj(file.file, buffer)
+    return input_path
