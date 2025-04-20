352f4ee8067343305d0dadfa675e5628c7f51d8e
Bychare <117040086+Bychare@users.noreply.github.com>
2025-04-20 17:39:59 +0300

    Update yolo_predict.py

--- model/yolo_predict.py
+++ model/yolo_predict.py
@@ -1,1 +1,10 @@
+from ultralytics import YOLO
 
+def load_model():
+    return YOLO("yolov8x.pt")
+
+def predict_image(model, image_path):
+    results = model(image_path)
+    output_path = image_path.replace(".jpg", "_result.jpg")
+    results[0].save(filename=output_path)
+    return output_path
