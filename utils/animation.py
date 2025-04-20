cc479a396c7179babcb0720dcddb173cc90df0ed
Bychare <117040086+Bychare@users.noreply.github.com>
2025-04-20 17:41:58 +0300

    Update animation.py

--- utils/animation.py
+++ utils/animation.py
@@ -1,1 +1,7 @@
+import imageio
+import matplotlib.pyplot as plt
+from glob import glob
 
+def create_gif(output_path="animated.gif", source="runs/detect/predict/*.jpg", duration=1):
+    imgs = [plt.imread(p) for p in sorted(glob(source))]
+    imageio.mimsave(output_path, imgs, duration=duration)
